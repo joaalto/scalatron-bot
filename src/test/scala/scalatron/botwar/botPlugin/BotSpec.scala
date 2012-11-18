b97212cd657d82bf7d9b17bd97b744ef77f5904d
Joonas Aalto <joaalto@iki.fi>
2012-11-18 20:51:40 +0200

    Added some view specs.

--- src/test/scala/scalatron/botwar/botPlugin/BotSpec.scala
+++ src/test/scala/scalatron/botwar/botPlugin/BotSpec.scala
@@ -4,11 +4,36 @@ import org.specs2.mutable._
 
 class BotSpec extends Specification {
 
-  val command = ""
+  /*
+   * React(generation=int,name=string,time=int,view=string,energy=string,master=int:int,collision=int:int,slaves=int,...)
+   */
+  def getViewStr(rowLength: Int) = "_" * rowLength * rowLength
+
+  def getView(rowLength: Int) = View(getViewStr(rowLength))
+
+  def getStandardView = getView(7)
+
+  def getCommand(viewRowLength: Int) = "React(generation=0,name=mybot,time=5,view=" +
+    getViewStr(viewRowLength) + ",energy=1000,collision=int:int,slaves=int,...)"
 
   step {
-
   }
 
-  "a commandParser"
+  "View center" should {
+    "equal (3, 3)" in {
+      getStandardView.center must beEqualTo(Xy(3, 3))
+    }
+  }
+
+  "View index" should {
+    "equal 6" in {
+      getView(5).indexFromAbsPos(Xy(1, 1)) must beEqualTo(6)
+    }
+  }
+
+  "Absolute position" should {
+    "equal (2, 3)" in {
+      getView(5).absPosFromIndex(17) must beEqualTo(Xy(2, 3))
+    }
+  }
 }
